d66543201b5508ec9d366623723949e420f7af74
Roman Ripari <romanripari@gmail.com>
2022-11-15 18:52:46 -0300

    Contact

--- js/functions.js
+++ js/functions.js
@@ -14,3 +14,16 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+$(document).mouseup(function(e) {
+  if (e.which != 1) return false;
+
+  $('.card--front').toggleClass('card--front--flip');
+  $('.card--back').toggleClass('card--back--flip');
+});
+
+
+$('a').mouseup(function(e) {
+  e.stopPropagation();
+});
+

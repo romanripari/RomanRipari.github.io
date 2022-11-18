ce7564da755a4dc979cf6379a45c7384eea6504c
Román Ripari <romanripari@gmail.com>
2022-11-18 16:53:59 -0300

    Infl

--- apps/infl.py
+++ apps/infl.py
@@ -14,3 +14,26 @@ def my_function(*args, **kwargs):
     return salida
 
 print(my_function())
+
+def SetMoneda(num, simbolo="US$", n_decimales=2):
+    n_decimales = abs(n_decimales)
+    num = round(num, n_decimales)
+    num, dec = str(num).split(".")
+    dec += "0" * (n_decimales - len(dec))
+    num = num[::-1]
+    l = [num[pos:pos+3][::-1] for pos in range(0,50,3) if (num[pos:pos+3])]
+    l.reverse()
+    num = str.join(",", l)
+    try:
+        if num[0:2] == "-,":
+            num = "-%s" % num[2:]
+    except IndexError:
+        pass
+    
+    if not n_decimales:
+        return "%s %s" % (simbolo, num)
+    return "%s %s.%s" % (simbolo, num, dec)
+
+
+
+

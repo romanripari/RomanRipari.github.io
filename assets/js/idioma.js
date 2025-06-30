0326837bc9ab94d76ebbe03a7e4f15d9e569ffcf
Roman Ripari <romanripari@gmail.com>
2025-06-30 00:29:12 -0300

    Arreglo idioma

--- assets/js/idioma.js
+++ assets/js/idioma.js
@@ -1,14 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+	var check = document.querySelector(".check");
+	check.addEventListener('click', idioma);
 
-var check=document.querySelector(".check");
-
-check.addEventListener('click', idioma);
-
-function idioma(){
-	let id = check.checked;
-	if (id == true){
-		location.href="es/es.html"
-	} else {
-		location.href="../"
+	function idioma() {
+		let id = check.checked;
+		if (id == true) {
+			location.href = "es/es.html";
+		} else {
+			location.href = "../";
+		}
 	}
-
-}
+});
